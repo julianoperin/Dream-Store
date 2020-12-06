995f5e5d08315428593cec6048949dc80ff8a8ac
‘Juliano <julianoperins@gmail.com>
2020-12-05 20:21:44 -0500

    Started working on the cart 2

--- development/about.720d5f78.js
+++ development/about.720d5f78.js
@@ -281,7 +281,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54133" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59379" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
